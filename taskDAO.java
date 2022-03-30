ba1dd33312fe074181d1a4a8fc004f65f84390e2
mounika-ammu <101688297+mounika-ammu@users.noreply.github.com>
2022-03-30 07:54:57 +0530

    Create taskDAO.java

---
+++ taskDAO.java
@@ -0,0 +1,11 @@
+# employeetask
+package com.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TaskDAO extends CrudRepository<Task, Integer> {
+
+}
