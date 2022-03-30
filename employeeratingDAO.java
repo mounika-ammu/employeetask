d10b6550c9d759052a15dea62e9e9efb18466b9e
mounika-ammu <101688297+mounika-ammu@users.noreply.github.com>
2022-03-30 07:52:44 +0530

    Create employeeratingDAO.java

---
+++ employeeratingDAO.java
@@ -0,0 +1,11 @@
+# employeetask
+package com.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRatingDAO extends CrudRepository<EmployeeRating, Integer> {
+
+}
